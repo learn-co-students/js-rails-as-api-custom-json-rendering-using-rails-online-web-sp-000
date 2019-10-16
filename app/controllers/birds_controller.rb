c35cf14c61b46910717edc8917ec4a840bdbc237
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-10-16 13:31:27 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,3 +4,80 @@ class BirdsController < ApplicationController
     render json: @birds
   end
 end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { message: 'Hashes of data will get converted to JSON' }
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: ['As','well','as','arrays']
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: @birds
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+#   end
+# end
+
+class BirdsController < ApplicationController
+  # def index
+  #   birds = Bird.all
+  #   render json: birds
+  # end
+
+  # def index
+  # birds = Bird.all
+  # render json: birds, only: [:id, :name, :species]
+  # end
+
+  def index
+  birds = Bird.all
+  render json: birds.to_json(except: [:created_at, :updated_at])
+ end
+
+ 
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   render json: bird
+  # end
+
+  # def show
+  # bird = Bird.find_by(id: params[:id])
+  # render json: {id: bird.id, name: bird.name, species: bird.species } 
+  # end
+
+  def show
+  bird = Bird.find_by(id: params[:id])
+  render json: {id: bird.id, name: bird.name, species: bird.species } 
+  # fhwhf
+  end
+
+end
