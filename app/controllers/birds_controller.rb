f07349a3626294ed5c3c4c75efc7bc88f17c9dcf
Eric Clark <eclark0013@gmail.com>
2020-03-16 16:07:39 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: @birds
+    render json: @birds, except: [:created_at]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: "Bird not found with id of #{params[:id]}" }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/birds' => 'birds#index'
+  resources :birds, only: [:index, :show]
+  # get '/birds' => 'birds#index'
 end
